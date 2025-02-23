d3443b7fe35c65e7a4362eac41b051bd5d21e623
Nitish kumar shah <nitish.zyn@gmail.com>
2025-02-23 23:19:42 +0530

    all scrolling are fixed and work well

--- js/home.js
+++ js/home.js
@@ -39,3 +39,31 @@ window.addEventListener("scroll", () => {
 
 //****************smooth scroll**************** */
 //select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    //prevent default
+    e.preventDefault();
+    //navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //calculate height
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linkContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linkContainer.style.height = 0;
+  });
+});
