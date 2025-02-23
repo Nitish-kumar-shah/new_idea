f0d72c7ad8eee0838e8bcaf09f49d6c2daa31915
Nitish kumar shah <nitish.zyn@gmail.com>
2025-02-23 20:19:40 +0530

    close link via toggle btn are complete

--- js/home.js
+++ js/home.js
@@ -1,3 +1,22 @@
 //***********set-date*************** */
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+//***********************close link*** */
+const navToggle = document.querySelector(".nav-toggle");
+const linkContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+  //   linkContainer.classList.toggle("show-links");
+  const containerHeight = linkContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linkContainer.style.height = `${linksHeight}px`;
+  } else {
+    linkContainer.style.height = 0;
+  }
+});
+
+//****************smooth scroll**************** */
+//select links
